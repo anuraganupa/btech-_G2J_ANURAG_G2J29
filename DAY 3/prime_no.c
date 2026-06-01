d1dca38ad8c0858270c6b8c96fa9a8679363b726
anuraganupa <om3346951yadav@gmail.com>
2026-06-01 20:37:36 +0530

    Add files via upload

---
+++ DAY 3/prime_no.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, s = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1) {
+        s = 0;
+    } else {
+        for (i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                s = 0;
+                break;
+            }
+        }
+    }
+
+    if (s)
+        printf("%d is a Prime Number", n);
+    else
+        printf("%d is Not a Prime Number", n);
+
+    return 0;
+}
